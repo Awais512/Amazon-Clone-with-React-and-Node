d710d14117d205234a113eb257d5d57a2b88bba1
awais512 <raza.awais25@gmail.com>
2021-11-30 14:44:52 +0500

    Adding product and rating component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Product from './components/Product';
+import data from './data';
 
 function App() {
+  const { products } = data;
+  console.log(products);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='grid-container'>
+      <header className='row'>
+        <div>
+          <a className='brand' href='index.html'>
+            Amazona
+          </a>
+        </div>
+        <div>
+          <a href='cart.html'>Cart</a>
+          <a href='signin.html'>Sign In</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div className='row center'>
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className='row center'>All right reserved</footer>
     </div>
   );
 }
