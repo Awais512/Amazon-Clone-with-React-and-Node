81e736843712431714fa1e0c9e9118121d1c3e50
awais512 <raza.awais25@gmail.com>
2021-11-30 16:51:51 +0500

    Load data from backend

--- src/screen/ProductScreen.js
+++ src/screen/ProductScreen.js
@@ -49,7 +49,7 @@ export default function ProductScreen(props) {
                     {product.countInStock > 0 ? (
                       <span className='success'>In Stock</span>
                     ) : (
-                      <span className='error'>Unavailable</span>
+                      <span className='error'>Out of Stock</span>
                     )}
                   </div>
                 </div>
